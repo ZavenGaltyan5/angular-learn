4d3f11e7f5180a8e10ce9a64702acde131a5ab03
vahan.martirosyan <vahan.martirosyan@connectto.com>
2022-10-14 14:14:24 +0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,69 @@ export class AppComponent {
 
   images = ['../assets/images/armenia.jpg', '../assets/images/usa.jpg', '../assets/images/armenia.jpg', '../assets/images/usa.jpg']
 
+
+  onInputValue(){
+    const input = document.getElementById('flag') as HTMLInputElement | null;
+    const addimg = document.getElementById('img1');
+    if (input?.value === 'Armenia' && addimg != null){
+      addimg.classList.add('img2');
+    }
+  }
+
+  onInputValue1(){
+    const input1 = document.getElementById('flag2') as HTMLInputElement | null;
+    const addimg1 = document.getElementById('img3');
+    if (input1?.value === 'Usa' && addimg1 != null) {
+      addimg1.classList.add('img3');
+    }
+  }
+
+  onInputValue2(){
+    const input2 = document.getElementById('flag3') as HTMLInputElement | null;
+    const addimg2 = document.getElementById('img4');
+    if (input2?.value === 'Russia' && addimg2 != null) {
+      addimg2.classList.add('img4');
+    }
+  }
+
+  onClickRemove(){
+    const pic1 = document.getElementById('img1');
+    const pic2 = document.getElementById('img3');
+    const pic3 = document.getElementById('img4');
+
+    if (pic1 != null){
+      pic1.classList.remove('img2')
+    }
+
+    if (pic2 != null){
+      pic2.classList.remove('img3')
+    }
+
+    if (pic3 != null){
+      pic3.classList.remove('img4')
+    }
+
+  }
+
+
 }
+
+// let flags = ['armenia','usa','russia'];
+
+
+const input = document.getElementById('flag') as HTMLInputElement | null;
+
+const value = input?.value;
+console.log(value) // 👉️ "Initial value"
+
+if (input != null) {
+  console.log(input.value); // 👉️ "Initial value"
+}
+
+input?.addEventListener('input', function (event) {
+  const target = event.target as HTMLInputElement;
+  console.log(target.value);
+});
 
 
 
