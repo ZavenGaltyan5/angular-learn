a900be6fb1b7c41922b9236dda9eb1fc667da96f
vahan.martirosyan <vahan.martirosyan@connectto.com>
2022-11-02 11:24:38 +0400

    .

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 
@@ -11,45 +11,45 @@ export class RegisterComponent implements OnInit {
 
   users = [];
   user = {};
-
+  local: any={};
   profileForm = new FormGroup({
 
-    firstName: new FormControl('',[
+    firstName: new FormControl('Zaven', [
       Validators.required,
       Validators.minLength(3),
     ]),
 
-    lastName: new FormControl('',[
+    lastName: new FormControl('Galstyan', [
       Validators.required,
       Validators.minLength(3),
     ]),
 
-    age: new FormControl('',[
+    age: new FormControl('20', [
       Validators.required,
       Validators.minLength(1),
     ]),
 
-    phone: new FormControl('',[
+    phone: new FormControl('99322522', [
       Validators.required,
       // Validators.pattern('^ [ + ] [0 - 9] $')
     ]),
 
-    email: new FormControl('',[
+    email: new FormControl('z.galstyanwork@gmail.com', [
       Validators.required,
       Validators.email,
     ]),
 
-    userName: new FormControl('',[
+    userName: new FormControl('Zaven55', [
       Validators.required,
       // Validators.pattern('^[a-z0-9_-]{3,15}$')
     ]),
 
-    pass: new FormControl('',[
+    pass: new FormControl('Zav1010', [
       Validators.required,
       Validators.minLength(5)
     ]),
 
-    confPass: new FormControl('',[
+    confPass: new FormControl('Zav1010', [
       Validators.required,
       Validators.minLength(5)
     ]),
@@ -63,58 +63,71 @@ export class RegisterComponent implements OnInit {
   }
 
 
-
-
-
   ShowPass() {
     // @ts-ignore
     const pas1 = document.getElementById('password');
     // @ts-ignore
-    if (pas1.type == 'password'){
+    if (pas1.type == 'password') {
       // @ts-ignore
       pas1.type = 'text';
-    }else {
+    } else {
       // @ts-ignore
       pas1.type = 'password'
     }
   }
+
   ShowPass2() {
     // @ts-ignore
     const pas2 = document.getElementById('confirm-pass');
     // @ts-ignore
-    if (pas2.type == 'password'){
+    if (pas2.type == 'password') {
       // @ts-ignore
       pas2.type = 'text';
-    }else {
+    } else {
       // @ts-ignore
       pas2.type = 'password'
     }
   }
-  PassAndConfPass(){
+
+  RegisterBtn() {
     const pas1 = document.getElementById('password') as HTMLInputElement | null;
     const pas2 = document.getElementById('confirm-pass') as HTMLInputElement | null;
-
-    if (pas1?.value != pas2?.value){
+    if (pas1?.value != pas2?.value) {
       alert('passwords are not equal')
     }
-
     this.user = {
-      firstName:this.profileForm.get('firstName')?.value,
-      lastName:this.profileForm.get('lastName')?.value,
-      userName:this.profileForm.get('userName')?.value,
-      email:this.profileForm.get('email')?.value,
-      phone:this.profileForm.get('phone')?.value,
-      age:this.profileForm.get('age')?.value,
-      password:this.profileForm.get('pass')?.value,
+      firstName: this.profileForm.get('firstName')?.value,
+      lastName: this.profileForm.get('lastName')?.value,
+      userName: this.profileForm.get('userName')?.value,
+      email: this.profileForm.get('email')?.value,
+      phone: this.profileForm.get('phone')?.value,
+      age: this.profileForm.get('age')?.value,
+      password: this.profileForm.get('pass')?.value,
     }
-
     // @ts-ignore
     this.users.push(this.user)
-
-    localStorage.setItem('user',JSON.stringify(this.users));
+    localStorage.setItem('user', JSON.stringify(this.users));
+    this.local = JSON.parse(localStorage.getItem('user') || '{}')
   }
 
 
+  LoginBtn(){
+    const uNameLogin = document.getElementById('login-input-name') as HTMLInputElement | null;
+    const uPassLogin = document.getElementById('login-input-pass') as HTMLInputElement | null;
+
+
+    for (let i=0;i<this.users.length;i++){
+      if (uNameLogin?.value === this.users[i]['userName'] && uPassLogin?.value === this.users[i]['password']){
+        alert('Login is Complete')
+      }
+
+    }
+
+
+
+
+
+  }
 
 
 
